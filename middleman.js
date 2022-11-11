882fb844233dc93c878252ad28301e93caa9272d
Electric131 <39623923+Electric131@users.noreply.github.com>
2022-11-11 17:19:46 -0600

    Update middleman.js

--- middleman.js
+++ middleman.js
@@ -23,7 +23,7 @@ wss.on('connection', function (ws, req) {
     console.log('Client connected.');
     console.log("Path: " + req.url);
     console.log("Connected Client Count: " + wss.clients.size);
-    console.log("Connected Clients: " + JSON.stringify(wss.clients));
+    console.log("Connected Clients: " + wss.clients);
 
     ws.on('close', function () {
         console.log('Client disconnected.');
